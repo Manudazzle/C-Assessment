a73cab194a2a05b83d96259a5c7ff6108139e74e
MANUKRISHNA M <manumanya637@gmail.com>
2026-03-09 11:36:16 +0530

    Update bitset_clear_toggle.c
    Changed the code as per the question. The program demonstrates these functions by specifically modifying the 3rd, 5th, and 2nd bit.

--- bitset_clear_toggle.c
+++ bitset_clear_toggle.c
@@ -1,62 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-//decimal to binary
-void Dec2Bin(int num);
 //bit setting
-void BitSet(int num,int pos);
+void BitSet(int *num,int pos);
 //bit clear
-void BitClear(int num,int pos);
+void BitClear(int *num,int pos);
 //bit toggle
-void BitToggle(int num, int pos);
+void BitToggle(int *num, int pos);
 
 int main() {
-    int num;
-    int pos;
-    printf("Enter a decimal number: ");
-    scanf("%d",&num);
-    printf("Binary value: ");
-    Dec2Bin(num);
-    printf("\n\nEnter a bit position to set: ");
-    scanf("%d",&pos);
-    printf("value of num after setting the bit: ");
-    BitSet(num,pos);
-    printf("\n\nEnter a bit position to clear: ");
-    scanf("%d",&pos);
-    BitClear(num,pos);
-    printf("\n\nEnter a bit position to Toggle: ");
-    scanf("%d",&pos);
-    BitToggle(num,pos);
-}
-void Dec2Bin(int num){
-    if (num==0)
-    {
-       printf("Binary= 0");
-    }
-    else{
-            for ( int i = 7; i>=0; i--)
-            {
-                if(num & (1<<i)){
-                    printf("1");
-                    }
-                    else{
-                        printf("0");
-                    }
-
-                }
-            }
-            
-    }
-void BitSet(int num,int pos){
-    num= num | 1<< pos;
-    Dec2Bin(num);
+	int num=0;
+	int pos;
+	printf("Given value:%d",num);
+	BitSet(&num,3);
+	printf("\nvalue of num after 3rd bit set:%d",num);
+	BitClear(&num,5);
+	printf("\nvalue of num after 5th bit clear:%d",num);
+	BitToggle(&num,2);
+	printf("\nvalue of num after 2nd bit toggle:%d",num);
+	return 0;
 }
 
-void BitClear(int num,int pos){
-    num= num &~ (1<< pos);
-    Dec2Bin(num);
+void BitSet(int *num,int pos) {
+	*num= *num | 1<< pos;
 }
 
-void BitToggle(int num,int pos){
-    num= num ^ 1<< pos;
-    Dec2Bin(num);
+void BitClear(int *num,int pos) {
+	*num= *num &~ (1<< pos);
+}
+
+void BitToggle(int *num,int pos) {
+	*num= *num ^ 1<< pos;
 }
